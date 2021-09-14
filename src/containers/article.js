bbad1531bf9b21b42b6c98508d28475e0f808c9a
ma4496 <ma4496@nyu.edu>
2021-09-14 08:57:23 -0400

    commit3

--- src/containers/article.js
+++ src/containers/article.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 function article(){
-    const articleTitle = 'single Article'
+    const articleTitle = 'single Article';
     return (
         <div>
-            <h1>{articleTitle)</h1>
+            <h1>{articleTitle}</h1>
         </div>
-    )
+    );
 }
 
 export default article;
