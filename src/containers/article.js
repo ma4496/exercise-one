7e2a1eedd4c581c256d00dc41b2cd10742b34565
ma4496 <ma4496@nyu.edu>
2021-09-14 08:45:00 -0400

    commit2

---
+++ src/containers/article.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function article(){
+    const articleTitle = 'single Article'
+    return (
+        <div>
+            <h1>{articleTitle)</h1>
+        </div>
+    )
+}
+
+export default article;
